cf00b49eb782c989291cf4e9fc5140ea17b182e9
Duncan McGreggor <oubiwann@gmail.com>
2016-07-26 18:26:45 -0500

    Added Clojure versions.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/clojusc/clj-starlanes"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.0.4"]]
   :plugins [[lein-exec "0.3.1"]
             [lein-kibit "0.1.2"]]
@@ -13,9 +13,13 @@
                    :integration :integration
                    :all (constantly true)}
   :profiles {
+    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
     :dev {
       :dependencies [[org.clojure/tools.namespace "0.2.3"]
-                     [org.clojure/java.classpath "0.2.0"]]}
-    :testing {
-      :dependencies [[clj-http-fake "0.4.1"]
-                     [leiningen "2.3.3"]]}})
+                     [org.clojure/java.classpath "0.2.0"]
+                     [clj-http-fake "0.4.1"]
+                     [leiningen "2.3.3"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.6:dev,1.7:dev,1.8:dev"]})
